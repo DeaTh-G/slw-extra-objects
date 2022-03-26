f8d142111e568b4b2385a2083675a9ff202193ca
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2022-03-27 00:41:38 +0100

    REALLY BAD UpReel logic code

--- Source/slw-extra-objects/slw-extra-objects/Object/CommonEx/UpReel/ObjUpReel.h
+++ Source/slw-extra-objects/slw-extra-objects/Object/CommonEx/UpReel/ObjUpReel.h
@@ -6,6 +6,10 @@ namespace app
     {
     protected:
         float m_Length{};
+        float m_Time{};
+        bool m_Move{};
+        bool m_LaunchUp{};
+        int m_PlayerNo{};
 
     public:
         ObjUpReel() {}
@@ -40,13 +44,10 @@ namespace app
 
                     pVisual->Setup(description);
 
-                    /*if (i == 1)
-                        pVisual->SetLocalScale({ 0.9f, 0.9f, 0.9f });*/
-
                     pVisualContainer->Add(pVisual);
                 }
                 
-                SetWireLength();
+                SetWireLength(-m_Length);
                 SetHandleDistance();
             }
 
@@ -87,45 +88,98 @@ namespace app
             }
         }
 
+        void Update(const fnd::SUpdateInfo& rInfo) override
+        {
+            if (!m_Move)
+                return;
+         
+            auto pHandleVisual = reinterpret_cast<fnd::GOCVisualModel*>(GetComponent<fnd::GOCVisualContainer>()->m_Visuals[1]);
+
+            m_Time += rInfo.deltaTime;
+            float position = m_Length - (m_Time * 550.0f);
+            if (position < 11.9f)
+            {
+                position = 11.9f;
+
+                xgame::MsgGetPosition playerPosMsg{};
+                ObjUtil::SendMessageImmToPlayer(*this, m_PlayerNo, playerPosMsg);
+
+                if (!m_LaunchUp)
+                {
+                    xgame::MsgSpringImpulse impulseMsg{ playerPosMsg.GetPosition(), static_cast<Vector3>(Vector3::UnitY() * 150.0f), 0.25f, 0.0f };
+                    ObjUtil::SendMessageImmToPlayer(*this, m_PlayerNo, impulseMsg);
+
+                    m_LaunchUp = true;
+                }
+                
+                auto* pPlayer = dynamic_cast<Player::CPlayer*>(m_pMessageManager->GetActor(ObjUtil::GetPlayerActorID(*m_pOwnerDocument, m_PlayerNo)));
+                float velocity = pPlayer->m_pPhysics->GetVertVelocity().y();
+
+                if (velocity > 20.0f)
+                    return;
+
+                m_Move = false;
+
+                xgame::MsgSpringImpulse impulse2ndMsg{ playerPosMsg.GetPosition(), static_cast<Vector3>(Vector3(0.0f, 0.5f, -0.5f) * 150.0f), 0.25f, 0.0f };
+                impulse2ndMsg.field_50.set(12);
+                ObjUtil::SendMessageImmToPlayer(*this, m_PlayerNo, impulse2ndMsg);
+            }
+
+            SetWireLength(-position);
+            pHandleVisual->SetLocalTranslation({ 0, -position, 0 });
+            GetComponent<game::GOCCollider>()->GetShape()->SetLocalTranslation({ 0, -position, 0 });
+        }
+
     private:
-        bool ProcMsgGetExternalMovePosition(xgame::MsgGetExternalMovePosition& message)
+        bool ProcMsgGetExternalMovePosition(xgame::MsgGetExternalMovePosition& msg)
         {
             auto* pHandleModel = reinterpret_cast<fnd::GOCVisualModel*>(GetComponent<fnd::GOCVisualContainer>()->m_Visuals[1]);
             if (!pHandleModel)
                 return false;
 
-            *message.m_pTransform = pHandleModel->m_Impl.m_Transform;
-            Quaternion rotation(pHandleModel->m_Impl.m_Transform);
+            pHandleModel->GetMatrix(msg.m_pTransform);
+
+            Quaternion rotation(*msg.m_pTransform);
             rotation = rotation * Eigen::AngleAxisf(TO_RAD(180), Vector3::UnitY());
             
             for (size_t i = 0; i < 3; i++)
-                message.m_pTransform->SetColumn(i, static_cast<Vector3>(rotation.toRotationMatrix().row(i)));
+                msg.m_pTransform->SetColumn(i, static_cast<Vector3>(rotation.toRotationMatrix().row(i)));
 
-            message.m_pTransform->GetTransVector().y() += -10.5f;
+            msg.m_pTransform->GetTransVector().y() += -10.5f;
+
+            return true;
         }
 
         bool ProcMsgHitEventCollision(xgame::MsgHitEventCollision& msg)
         {
-            int playerNo = ObjUtil::GetPlayerNo(*m_pOwnerDocument, msg.m_Sender);
-            if (playerNo < 0)
+            m_PlayerNo = ObjUtil::GetPlayerNo(*m_pOwnerDocument, msg.m_Sender);
+            if (m_PlayerNo < 0)
                 return false;
+
+            m_Move = true;
 
             xgame::MsgCatchPlayer catchMessage{};
             catchMessage.m_Unk3 = 13;
-            ObjUtil::SendMessageImmToPlayer(*this, playerNo, catchMessage);
+            ObjUtil::SendMessageImmToPlayer(*this, m_PlayerNo, catchMessage);
 
             return true;
         }
 
         bool ProcMsgPLGetHomingTargetInfo(xgame::MsgPLGetHomingTargetInfo& msg)
         {
-            auto cursorPos = GetComponent<fnd::GOCTransform>()->m_Frame.m_Unk1.m_Mtx * Vector4(0, -m_Length - 4.0f, 0, 1);
-            msg.m_CursorPosition = Vector3(cursorPos.x(), cursorPos.y(), cursorPos.z());
+            auto* pHandleModel = reinterpret_cast<fnd::GOCVisualModel*>(GetComponent<fnd::GOCVisualContainer>()->m_Visuals[1]);
+            if (!pHandleModel)
+                return false;
+            
+            Matrix34 handleTransform{};
+            pHandleModel->GetMatrix(&handleTransform);
+            
+            msg.m_CursorPosition = handleTransform.GetTransVector();
 
             return true;
         }
 
-        void SetWireLength()
+        void SetWireLength(float length)
         {
             auto* pWireModel = reinterpret_cast<fnd::GOCVisualModel*>(GetComponent<fnd::GOCVisualContainer>()->m_Visuals[2]);
             if (!pWireModel)
@@ -134,10 +188,12 @@ namespace app
             Transform transform{};
 
             pWireModel->GetNodeTransform(2, "Cos_top", &transform);
-            transform.m_Position.y() += -m_Length + 4.0f;
+            transform.m_Position.y() = length + 6.0f;
             pWireModel->SetNodeTransform(2, "Cos_top", transform);
             
-            pWireModel->m_Impl.m_pModelNode->m_Bounds.m_Max.y() += -m_Length + 4.0f;
+            auto bounds = pWireModel->GetBounds();
+            bounds.m_Max.y() = length + 6.0f;
+            pWireModel->SetBounds(bounds);
         }
 
         void SetHandleDistance()
