4a286e17e746d27f9b55b8c2a1c83e990665a515
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-20 00:18:51 +0200

    Organize patch includes

---
+++ Source/slw-extra-objects/slw-extra-objects/Patches.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Patches/Stage/StageDataInitializer.h"
+
+#include "Patches/System/Player/PlayerStateBase.h"
+#include "Patches/System/Player/State/StateCaught.h"
+#include "Patches/System/Player/PlayerResourceInfoRegister.h"
+#include "Patches/System/Player/VisualSonic.h"
+#include "Patches/System/Player/Info/PlayerVisualSonicInfo.h"
+
+#include "Patches/Object/objectObjectReflections.h"
+#include "Patches/System/ObjectSystem/SetObjectFactory.h"
