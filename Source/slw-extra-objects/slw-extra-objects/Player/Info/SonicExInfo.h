280fb25e1b3e7ce919d70855f1a0883e96256f8f
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-19 21:44:37 +0200

    Update to LWAPI style namings

--- Source/slw-extra-objects/slw-extra-objects/Player/Info/SonicExInfo.h
+++ Source/slw-extra-objects/slw-extra-objects/Player/Info/SonicExInfo.h
@@ -4,35 +4,43 @@ namespace app::Player
 {
     class SonicExInfo : public CObjInfo
     {
+    private:
+        inline static const char* ms_pBasePackfileName = "Sonic.pac";
+        inline static const char* ms_pPackfileName = "SonicEx.pac";
+        inline static const char* ms_pSkeletonName = "chr_Sonic";
+        inline static const char* ms_pAnimationScriptName = "player_sonic_ex";
+
     public:
-        hh::gfx::res::ResSkeleton m_Skeleton {};
-        animation::AnimationResContainer m_AnimContainer{ *m_pAllocator };
+        inline static const char* ms_pName = "SonicExInfo";
+        
+        hh::gfx::res::ResSkeleton Skeleton {};
+        animation::AnimationResContainer AnimationContainer{ *m_pAllocator };
 
         SonicExInfo() {}
 
     protected:
         void Load(CObjInfoFileLoader& loader) override
         {
-            loader.Load("SonicEx.pac", 0x4001);
+            loader.Load(ms_pPackfileName, 0x4001);
         }
 
         void Initialize(GameDocument& document) override
         {
             hh::ut::PackFile packFile{ nullptr };
 
-            packFile = ObjUtil::GetPackFile("Sonic.pac");
-            m_Skeleton = ObjUtil::GetSkeletonResource("chr_Sonic", packFile);
+            packFile = ObjUtil::GetPackFile(ms_pBasePackfileName);
+            Skeleton = ObjUtil::GetSkeletonResource(ms_pSkeletonName, packFile);
 
-            packFile = ObjUtil::GetPackFile("SonicEx.pac");
-            auto animScript = ObjUtil::GetAnimationScriptResource("player_sonic_ex", packFile);
+            packFile = ObjUtil::GetPackFile(ms_pPackfileName);
+            auto animScript = ObjUtil::GetAnimationScriptResource(ms_pAnimationScriptName, packFile);
 
-            animation::AnimationResContainer::LoadInfo loadInfo{ animScript, m_Skeleton, nullptr };
-            m_AnimContainer.LoadFromBuffer(loadInfo, packFile);
+            animation::AnimationResContainer::LoadInfo loadInfo{ animScript, Skeleton, nullptr };
+            AnimationContainer.LoadFromBuffer(loadInfo, packFile);
         }
 
         const char* GetInfoName() override
         {
-            return "SonicExInfo";
+            return ms_pName;
         }
     };
 }
