9c0ef11e2bd0915f854bbbc6e21b591023076d2a
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-20 00:18:31 +0200

    Remove redudant includes

--- Source/slw-extra-objects/slw-extra-objects/slw-extra-objects.h
+++ Source/slw-extra-objects/slw-extra-objects/slw-extra-objects.h
@@ -1,35 +1,5 @@
 #pragma once
 
 #include "utils.h"
-
-// SonicEx
-#include "Player/Info/SonicExInfo.h"
-
-// Dash Ring
-#include "Object/CommonEx/DashRing/DashRingParam.h"
-#include "Object/CommonEx/DashRing/ObjDashRingInfo.h"
-#include "Object/CommonEx/DashRing/ObjDashRing.h"
-
-// Jump Panel
-#include "Object/CommonEx/JumpPanel/JumpPanelParam.h"
-#include "Object/CommonEx/JumpPanel/ObjJumpPanelInfo.h"
-#include "Object/CommonEx/JumpPanel/ObjJumpPanel.h"
-
-// Jump Board Extra
-#include "Object/CommonEx/JumpBoardEx/JumpBoardExParam.h"
-#include "Object/CommonEx/JumpBoardEx/ObjJumpBoardExInfo.h"
-#include "Object/CommonEx/JumpBoardEx/ObjJumpBoardEx.h"
-
-// Super Ring Super
-#include "Object/CommonEx/SuperRingSuper/ObjSuperRingSuperInfo.h"
-#include "Object/CommonEx/SuperRingSuper/ObjSuperRingSuper.h"
-
-// Wide Spring
-#include "Object/CommonEx/WideSpring/WideSpringParam.h"
-#include "Object/CommonEx/WideSpring/ObjWideSpringInfo.h"
-#include "Object/CommonEx/WideSpring/ObjWideSpring.h"
-
-// Wide Spring
-#include "Object/CommonEx/UpReel/UpReelParam.h"
-#include "Object/CommonEx/UpReel/ObjUpReelInfo.h"
-#include "Object/CommonEx/UpReel/ObjUpReel.h"
+#include "Expanded.h"
+#include "Patches.h"
