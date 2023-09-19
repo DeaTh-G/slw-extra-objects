6162eacb7d7de0fd8d4e84d5ad59e506a2a99d76
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-20 00:25:42 +0200

    Update Mod.cpp

--- Source/slw-extra-objects/slw-extra-objects/Mod.cpp
+++ Source/slw-extra-objects/slw-extra-objects/Mod.cpp
@@ -1,15 +1,17 @@
 #include "pch.h"
-#include "LostCodeLoader.h"
-#include "Patches\Patches.h"
+#include "ModLoader.h"
 
 extern "C"
 {
-	void _declspec(dllexport) __cdecl Init(ModInfo* modInfo)
+	void _declspec(dllexport) __cdecl Init(ModInfo_t* modInfo)
 	{
 		MessageBoxA(nullptr, "", "", 0);
-		SLW_EXTRA_OBJECTS::CSetObjectFactory::InstallPatches();
-		SLW_EXTRA_OBJECTS::StageDataInitializer::InstallPatches();
-		SLW_EXTRA_OBJECTS::CStateBase::InstallPatches();
-		SLW_EXTRA_OBJECTS::Player::SonicInfo::InstallPatches();
+
+		//slw_extra_objects::CHudGameMainDisplay::InstallPatches();
+		slw_extra_objects::CSetObjectFactory::InstallHooks();
+		slw_extra_objects::StageDataInitializer::InstallHooks();
+		//slw_extra_objects::Player::CStateActionBase::InstallHooks();
+		//slw_extra_objects::Player::CStateCaught::InstallHooks();
+		//slw_extra_objects::Player::SonicInfo::InstallHooks();
 	}
 }
