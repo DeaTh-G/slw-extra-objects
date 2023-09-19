29702affe462b44e9edd78ec498bcb15ebaefecc
DeaTh-G <55578911+DeaTh-G@users.noreply.github.com>
2023-09-20 00:26:09 +0200

    Add back WRITE_JUMP macro

--- Source/slw-extra-objects/slw-extra-objects/utils.h
+++ Source/slw-extra-objects/slw-extra-objects/utils.h
@@ -69,3 +69,9 @@
             } \
         } \
     }
+
+#define WRITE_JUMP(location, function) \
+    { \
+        WRITE_MEMORY_WITH_TYPE(location, uint8_t, 0xE9); \
+        WRITE_MEMORY_WITH_TYPE(location + 1, uint32_t, (uint32_t)(function) - (size_t)(location) - 5); \
+    }
